46ae5c3c6754bdeddc1c8c523ccb1b2c5c27a2da
Ilya Dyakonov <iluskadyakonov@gmail.com>
2014-04-19 17:38:34 +0200

    blank project

---
+++ RevealViewController/ZLRevealViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZLRevealViewController.h
+//  RevealViewControllerTest
+//
+//  Created by Ilya Dyakonov on 19/04/14.
+//  Copyright (c) 2014 ZappyLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZLRevealViewController : UIViewController
+
+@end
---
+++ RevealViewControllerTest/RevealViewControllerTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RevealViewControllerTest
+//
+//  Created by Ilya Dyakonov on 19/04/14.
+//  Copyright (c) 2014 ZappyLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
